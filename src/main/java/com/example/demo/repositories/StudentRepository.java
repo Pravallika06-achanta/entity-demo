f0fc444875d2af92f4e3904fde7433dfa36ccc03
Pravallika06-achanta <pravallikaachanta45464@gmail.com>
2025-06-11 00:31:46 +0530

    Update StudentRepository.java

--- src/main/java/com/example/demo/repositories/StudentRepository.java
+++ src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -1,1 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
 
