90f9250ff00048e7db32cc223256da1b7585decb
Pravallika06-achanta <pravallikaachanta45464@gmail.com>
2025-06-11 00:34:27 +0530

    Update StudentController.java

--- src/main/java/com/example/demo/repositories/src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/repositories/src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,1 +1,29 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Student;
+import com.example.demo.repositories.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository repository;
+
+    public StudentController(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Student create(@RequestBody Student student) {
+        return repository.save(student);
+    }
+}
 
